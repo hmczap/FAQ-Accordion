c0745d3b4c303e3b1f1778882a9ba1ee48eca2a7
hmczap <114511170+hmczap@users.noreply.github.com>
2024-03-06 12:23:48 -0500

    Initialize tailwind config file and tailwind stylesheet

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [],
+  purge:['./resources/**/*.{js,jsx,ts,tsx}','./index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
