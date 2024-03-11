a53cbe1bb83313ec6e09d380078ac192f0736272
hmczap <114511170+hmczap@users.noreply.github.com>
2024-03-10 23:23:29 -0400

    Extend tailwind.config with background images and custom spacing properties for mobile styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,18 @@ module.exports ={
   content: ["./*.{html,js}", "./**/*.{html,js}","./resources/**/*.js"],
   purge:['./resources/**/*.{js,jsx,ts,tsx}','./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'body': "url('./resources/images/background-pattern-desktop.svg')",
+        'body-mobile': "url('./resources/images/background-pattern-mobile.svg')"
+      },
+      spacing: {
+        '58': '14.5rem'
+      }
+    },
+    screens: {
+      mobile: {'max': '375px'}
+    },
   },
   plugins: [],
 }
