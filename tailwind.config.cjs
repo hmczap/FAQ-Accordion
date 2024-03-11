25fcfaebeb7e56d65d995de548a727da8c5075c9
hmczap <114511170+hmczap@users.noreply.github.com>
2024-03-10 23:54:49 -0400

    Update tailwind.config and index.html to include font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,15 @@ module.exports ={
         'body': "url('./resources/images/background-pattern-desktop.svg')",
         'body-mobile': "url('./resources/images/background-pattern-mobile.svg')"
       },
+      backgroundColor: {
+        'white': "hsl(0, 0%, 100%)",
+        'light-pink': "hsl(275, 100%, 97%)",
+        'grayish-purple': "hsl(292, 16%, 49%)",
+        'dark-purple' : "hsl(292, 42%, 14%)"
+      },
+      fontFamily: {
+        'work-sans': ["Work-Sans", "sans-serif"]
+      },
       spacing: {
         '58': '14.5rem'
       }
