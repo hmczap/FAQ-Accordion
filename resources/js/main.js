b0eec6537e3dcc0ab65f3e63b4fca4d8f9fe0063
hmczap <114511170+hmczap@users.noreply.github.com>
2024-03-31 17:32:18 -0400

    Added expand icons to each FAQ element

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,20 +3,23 @@
 var accordion = document.getElementsByClassName('accordion');
 var i;
 
-console.log("loaded script")
-
 function toggleAccordion(){
     this.classList.toggle("active");
 
     var panel = this.nextElementSibling;
+    var icon = this.children[1];
+
     if (panel.style.maxHeight){
         panel.style.maxHeight = null;
         panel.style.marginTop = "0px";
+        // toggle icon = minus
     } else {
         panel.style.maxHeight = panel.scrollHeight + "px";
         panel.style.marginTop = "18px";
+        // toggle icon = plus
     }
 }
+
 
 for (i = 0; i < accordion.length; i++){
     accordion[i].addEventListener("click", toggleAccordion);
