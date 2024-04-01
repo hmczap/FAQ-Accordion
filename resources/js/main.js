3b32dcb2c755577039459ee0cb56d5a2bd5b4db6
hmczap <114511170+hmczap@users.noreply.github.com>
2024-04-01 17:11:09 -0400

    Modify main.js to toggle plus/minus icon image sources on click

--- resources/js/main.js
+++ resources/js/main.js
@@ -7,19 +7,24 @@ function toggleAccordion(){
     this.classList.toggle("active");
 
     var panel = this.nextElementSibling;
-    var icon = this.children[1];
+    var icon = this.children[1].children[0];
 
     if (panel.style.maxHeight){
         panel.style.maxHeight = null;
         panel.style.marginTop = "0px";
-        // toggle icon = minus
+
+        icon.src = "./resources/images/icon-plus.svg";
+        icon.alt = "minus icon";
+
     } else {
         panel.style.maxHeight = panel.scrollHeight + "px";
         panel.style.marginTop = "18px";
-        // toggle icon = plus
+
+        icon.src = "./resources/images/icon-minus.svg";
+        icon.alt = "plus icon";
+        
     }
 }
-
 
 for (i = 0; i < accordion.length; i++){
     accordion[i].addEventListener("click", toggleAccordion);
